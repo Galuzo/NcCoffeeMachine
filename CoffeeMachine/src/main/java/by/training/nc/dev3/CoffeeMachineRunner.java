841fe9ddcd99a3151f22259fdf99a3b26c48e8f1
benderoken <benderoken@gmail.com>
2017-03-18 15:59:06 +0300

    initial Commit of CoffeeMachine

---
+++ CoffeeMachine/src/main/java/by/training/nc/dev3/CoffeeMachineRunner.java
@@ -0,0 +1,10 @@
+package by.training.nc.dev3;
+
+/**
+ * Created by Win on 18.03.2017.
+ */
+public class CoffeeMachineRunner {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
