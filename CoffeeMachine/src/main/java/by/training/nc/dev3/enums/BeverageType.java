2ba5e20ad1b7d78bb8d7278fb25bfabf1df5d298
benderoken <benderoken@gmail.com>
2017-03-22 15:16:35 +0300

    #DEV3_GALUZO-13

--- CoffeeMachine/src/main/java/by/training/nc/dev3/enums/BeverageType.java
+++ CoffeeMachine/src/main/java/by/training/nc/dev3/enums/BeverageType.java
@@ -4,5 +4,17 @@ package by.training.nc.dev3.enums;
  * Created by Win on 19.03.2017.
  */
 public enum BeverageType {
-    LATTE,TEA
+    LATTE,TEA;
+    public String getBeverageType(){
+        switch(this){
+            case LATTE:
+                return "latte";
+            case TEA:
+                return "tea";
+
+            default:
+                return "Indefined";
+
+        }
+    }
 }
--- CoffeeMachine/src/main/java/by/training/nc/dev3/enums/IngredientType.java
+++ CoffeeMachine/src/main/java/by/training/nc/dev3/enums/IngredientType.java
@@ -4,5 +4,17 @@ package by.training.nc.dev3.enums;
  * Created by Win on 19.03.2017.
  */
 public enum IngredientType {
-    CHOCOLATE,MILK
+    CHOCOLATE,MILK;
+
+    public String getIngredientType(){
+        switch(this){
+            case CHOCOLATE:
+                return "chocolate";
+            case MILK:
+                return "milk";
+
+            default:
+                return "Indefined";
+        }
+    }
 }
